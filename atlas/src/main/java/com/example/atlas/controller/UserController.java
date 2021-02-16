40b31c4cc009afc8d59752c5c5b3765eaea3689f
SilvaBuenoVitor <vitot770@gmail.com>
2021-02-16 15:03:45 -0300

    added basic user

---
+++ atlas/src/main/java/com/example/atlas/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.atlas.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.example.atlas.model.User;
+import com.example.atlas.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping
+    public ResponseEntity login(@RequestBody User user, HttpServletRequest request, UriComponentsBuilder builder){
+        if (repository.login(user)){
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ atlas/src/main/java/com/example/atlas/repository/UserRepository.java
@@ -0,0 +1,36 @@
+package com.example.atlas.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import com.example.atlas.model.User;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRepository {
+    private List<User> users;
+    private int nextId = 1;
+    
+    @PostConstruct
+    public void init(){
+        User u1 = new User();
+        u1.setId(0);
+        u1.setEmail("email");
+        u1.setPassword("password");
+
+        users = new ArrayList<User>();
+        users.add(u1);
+    }
+
+    public boolean login(User login){
+        for (User user : users) {
+            if(user.getEmail().equals(login.getEmail()) && user.getPassword().equals(login.getPassword())){
+                return true;
+            }
+        }
+        return false;
+    }
+}
